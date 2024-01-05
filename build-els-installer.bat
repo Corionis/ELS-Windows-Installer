4cef04dbea4e0fba0d4067ad55c915d5acf90aeb
Corionis <rockney8588@gmail.com>
2024-01-05 09:40:33 -0700

    #67 Minor batch change

--- build-els-installer.bat
+++ build-els-installer.bat
@@ -4,14 +4,9 @@ echo/
 echo Build ELS Windows Installer
 echo/
 
-set /p "yn=Has the out/ELS/zip/ELS/ directory been copied from Linux (y/N)? "
-if %yn%z==z set yn=n
-if %yn%==Y set yn=y
-
-if not %yn%==y goto Done
 echo/
 echo Building ...
 "C:\Program Files (x86)\Actual Installer\actinst.exe" /S "Entertainment Library Synchronizer.aip"
+if not %ERRORLEVEL% == 0 echo\ && echo BUILD FAILED
 
-:Done
 echo/
